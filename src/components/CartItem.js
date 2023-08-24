7b1ef7f66efb25d12b08202a7a83c7e34179ab5c
Mitesh Prajapati <mp5186349@gmail.com>
2023-08-24 18:45:42 +0530

    Shopping With Me

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,43 +1,41 @@
-import React from 'react'
-import { toast } from "react-hot-toast"
-import {FcDeleteDatabase} from "react-icons/fc"
-import { useDispatch } from "react-redux"
-import { remove } from "../redux/slices/CartSlice"
+import React from 'react';
+import { toast } from 'react-hot-toast';
+import { FcDeleteDatabase } from 'react-icons/fc';
+import { useDispatch } from 'react-redux';
+import { remove } from '../redux/slices/CartSlice';
 
+const CartItem = ({ item, itemIndex }) => {
+  const dispatch = useDispatch();
 
-const CartItem = ( {item,itemIndex}) => {
-    const dispatch = useDispatch();
-
-  const removeFromCart = ()=>{
+  const removeFromCart = () => {
     dispatch(remove(item.id));
-    toast.error("Item Removed ");
-  }
+    toast.error('Item Removed');
+  };
 
   return (
-    <div >
-       <div className='flex flex-row gap-9' >
+    <div className="flex flex-col md:flex-row border rounded-md p-4 mb-4">
+      <div className="md:w-1/4">
+        <img src={item.image} alt={item.title} className="w-full md:max-w-xs mx-auto md:mx-0" />
+      </div>
 
-          <div className='max-h-44 max-w-[150px] mt-16 ml-32'>
-            <img src={item.image} className='h-full w-full' />
+      <div className="md:w-3/4 md:pl-4">
+        <div className="flex flex-col md:flex-row md:justify-between">
+          <div className="mb-2 md:mb-0">
+            <h1 className="text-xl font-semibold mb-2">{item.title}</h1>
+            <p className="text-gray-600">
+              {item.description.split(' ').slice(1, 10).join(' ') + '...'}
+            </p>
           </div>
-
-          <div className='flex flex-col gap-3 mt-16 max-w-[300px]'>
-            <h1 className='font-bold text-[1.5rem]'>{item.title}</h1>
-            <h1 className='w-full text-gray-600 font-normal text-[13px] text-left'>{item.description.split(" ").slice(1,10).join(" ") + "..."}</h1>
-            <div>
-                <p className='mb-3 text-green-600 font-semibold'>${item.price}</p>
-                <div onClick={removeFromCart}>
-                    <FcDeleteDatabase className='text-[2rem]  border shadow'/>
-                </div>
-
+          <div className="flex items-center">
+            <p className="text-xl font-semibold">${item.price}</p>
+            <div className="cursor-pointer ml-4" onClick={removeFromCart}>
+              <FcDeleteDatabase className="text-red-500" />
             </div>
-           
           </div>
-
-       </div>
-       <div className='h-[2px] bg-slate-600 mt-6 ml-36 w-[500px]'></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,75 +1,60 @@
-import React from 'react'
-import { useSelector } from "react-redux"
-import { Link } from "react-router-dom"
-import CartItem from '../components/CartItem'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import CartItem from '../components/CartItem';
+import { useState, useEffect } from 'react';
 
 const Cart = () => {
+  const { cart } = useSelector((state) => state);
+  const [totalAmount, setTotalAmount] = useState(0);
 
-    const { cart } = useSelector((state) => state)
-    const [totalAmount, setTotalAmount] = useState(0);
+  useEffect(() => {
+    setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart]);
 
-    useEffect(() => {
-        setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
-    }, [cart])
+  return (
+    <div className="flex flex-col md:flex-row justify-center items-center md:space-x-8 md:px-8">
+      {cart.length > 0 ? (
+        // Cart has items
+        <div className="md:flex md:space-x-4">
+          <div className="md:w-3/4">
+            {cart.map((item, index) => (
+              <CartItem key={item.id} item={item} itemIndex={index} />
+            ))}
+          </div>
 
-    return (
-        <div className='flex justify-center items-center'>
-            {
-                (cart.length > 0) ?
-                    (<div className='flex flex-row '>
+          <div className="md:w-1/4 mt-10 md:mt-0">
+            <div>
+              <div className="font-bold text-lg text-green-600">Your Cart</div>
+              <div className="font-bold text-2xl text-green-600">Summary</div>
+              <p className="font-bold text-lg">
+                <span>Total Items: {cart.length}</span>
+              </p>
+            </div>
 
-                        <div>
-                            {
-                                cart.map((item, index) => {
-                                    return <CartItem
-                                        key={item.id} item={item} itemIndex={index} />
-                                })
-                            }
-                        </div>
+            <div className="mt-4">
+              <p className="text-slate-700 font-medium">
+                Total Amount: <span className="font-extrabold">${totalAmount}</span>
+              </p>
+              <button className="text-white text-xl bg-green-700 rounded-lg p-2 mt-4">
+                Checkout Now
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        // Cart is empty
+        <div className="flex flex-col items-center mt-20">
+          <h2 className="font-semibold text-xl mb-3">Your Cart is Empty</h2>
+          <Link to="/">
+            <button className="text-white text-xl bg-green-700 rounded-lg p-2">
+              Shop Now
+            </button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
 
-                        {/* SummaryPart */}
-
-                        <div className='mt-20 ml-28' >
-                            <div>
-                                <div className='font-bold text-lg text-green-600'>Your Cart </div>
-                                <div className='font-bold text-[2rem]  text-green-600'>Summary</div>
-                                <p className='font-bold text-lg '>
-                                    <span>Total Items: {cart.length}</span>
-                                </p>
-                            </div>
-
-
-
-                            {/* checkoutPart */}
-                            <div>
-                                <p className='text-slate-700 font-medium'>Total Amount: <span className='font-extrabold'>${totalAmount}</span></p>
-                                <button className='text-white text-[1.5rem] bg-green-700 rounded-lg p-1 '>
-                                    checkout Now
-                                </button>
-                            </div>
-
-                        </div>
-
-                    </div>) :
-
-
-                    (
-                        //   EmptyPart
-                        <div className='justify-center mt-80'>
-                            <h2 className='font-semibold text-[1.5rem] mb-3'>
-                                Your Cart is Empty
-                            </h2>
-                            <Link to="/">
-                                <button className='text-white text-[1.5rem] bg-green-700 rounded-lg p-1 ml-9'>
-                                    Shop Now
-                                </button>
-                            </Link>
-                        </div >)
-            }
-        </div >
-    )
-}
-
-export default Cart
+export default Cart;
